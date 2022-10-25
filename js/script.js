e100d8206dcb39374dde8caaf207d211fb6003ed
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-25 07:04:53 -0500

    [add] milestone 0

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-// Wayne Barnett	Founder & CEO			wayne-barnett-founder-ceo.jpg
-// Angela Caroll	        Chief Editor			        angela-caroll-chief-editor.jpg
-// Walter Gordon	Office Manager			walter-gordon-office-manager.jpg
-// Angela Lopez	        Social Media Manager	        angela-lopez-social-media-manager.jpg
-// Scott Estrada	        Developer				scott-estrada-developer.jpg
-// Barbara Ramos	Graphic Designer		        barbara-ramos-graphic-designer.jpg
 // **MILESTONE 0:**
 // Creare l’array di oggetti con le informazioni fornite.
 // **MILESTONE 1:**
@@ -16,7 +8,41 @@
 // Stampare delle card formattate contenete immagini e testo (La grafica fornita è solo un esempio ma sentitevi liberi di “graficare” come preferite)
 // **BONUS:**
 // Aggiungere degli aggettivi (più di uno) che identifichino ogni persona e stamparli nella card
-// **Consigli del giorno:**
-// Ragioniamo come sempre a step.
-// Prima la logica in italiano e poi traduciamo in codice.
-// E ricordiamoci che `console.log()` è nostro amico!
+
+
+
+// **MILESTONE 0:**
+// Creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg"
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "scott-estrada-developer.jpg"
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "barbara-ramos-graphic-designer.jpg"
+  }
+];
