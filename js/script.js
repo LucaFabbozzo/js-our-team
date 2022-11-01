93a3065856cc3d99c5e039aa4efade286ed19cc9
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-01 08:07:09 -0500

    refactoring

--- js/script.js
+++ js/script.js
@@ -10,71 +10,121 @@
 // Aggiungere degli aggettivi (più di uno) che identifichino ogni persona e stamparli nella card
 
 
-const output = document.querySelector('.row');
-
-
 const team = [
   {
     name: "Wayne Barnett",
     role: "Founder & CEO",
-    photo: "img/wayne-barnett-founder-ceo.jpg"
+    photo: "img/wayne-barnett-founder-ceo.jpg",
+    qualities: ['Great Team Leader',
+    'always Untiring'
+    ]
   },
   {
     name: "Angela Caroll",
     role: "Chief Editor",
-    photo: "img/angela-caroll-chief-editor.jpg"
+    photo: "img/angela-caroll-chief-editor.jpg",
+    qualities: ['Unconventional Girl']
   },
   {
     name: "Walter Gordon",
     role: "Office Manager",
-    photo: "img/walter-gordon-office-manager.jpg"
+    photo: "img/walter-gordon-office-manager.jpg",
+    qualities: ['With clear Ideas']
   },
   {
     name: "Angela Lopez",
     role: "Social Media Manager",
-    photo: "img/angela-lopez-social-media-manager.jpg"
+    photo: "img/angela-lopez-social-media-manager.jpg",
+    qualities: ['Little Angie']
   },
   {
     name: "Scott Estrada",
     role: "Developer",
-    photo: "img/scott-estrada-developer.jpg"
+    photo: "img/scott-estrada-developer.jpg",
+    qualities: ['Problem Solver']
   },
   {
     name: "Barbara Ramos",
     role: "Graphic Designer",
-    photo: "img/barbara-ramos-graphic-designer.jpg"
+    photo: "img/barbara-ramos-graphic-designer.jpg",
+    qualities: ['Always Smiling']
   }
 ];
 
 
-//aggettivi
-team[0].adjectives = "Great Team Leader always Untiring";
-team[1].adjectives = "Unconventional Girl";
-team[2].adjectives = "With clear Ideas";
-team[3].adjectives = "Little Angie";
-team[4].adjectives = "Problem Solver";
-team[5].adjectives = "Always Smiling";
 
 
+const teamContainer = document.querySelector('.row');
+const addButton = document.querySelector('#btnAdd');
+addButton.addEventListener('click', addNewMember);
 
-for(let infos of team) {
+printTeam();
 
-  
-  let card = `
+
+function printTeam() {
+  //resetto il teamContainer
+  teamContainer.innerHTML = '';
+
+  //stampo tutti i membri del mio team
+  team.forEach(teamMember => drawTeamMember(teamMember))
+
+}
+
+function drawTeamMember(teamMember) {
+    //Leggo il contenuto del container
+  let prevContent = teamContainer.innerHTML;
+
+    //prendo le informazione dal teamMember
+  const name = teamMember.name;
+  const role = teamMember.role;
+  const photo = teamMember.photo;
+  const qualities = teamMember.qualities;
+
+   //genero l'autput html
+    prevContent += `
   <div class="col py-4">
-    <div class="card" style="width: 20em; cursor: pointer; transition-duration: 0.5s;" onmouseover="this.style.transform='scale(1.05)';"
-    onmouseout="this.style.transform='scale(1)';">
-      <img src="${infos.photo}" class="card-img-top">
+    <div class="card" style="width: 20em;">
+      <img src="${photo}" class="card-img-top">
       <div class="card-body">
-        <h5 class="card-title text-center">${infos.name}</h5>
-        <p class="card-text text-center">${infos.role}</p>
-        <p class="card-adjectives text-center" style="font-size: 0.9rem">${infos.adjectives}</p>
+        <h5 class="card-title text-center">${name}</h5>
+        <p class="card-text text-center">${role}</p>
+        <p class="card-adjectives text-center" style="font-size: 0.9rem">${qualities}</p>
       </div>
     </div>
   </div>
  `;
 
-  output.innerHTML += card;
-
+  //lo aggiungo  all'html presente
+  teamContainer.innerHTML = prevContent;
 }
 
+
+function addNewMember() {
+ 
+  //Leggo il valore degli input
+  const name = document.getElementById('name').value;
+  const role = document.getElementById('role').value;
+  const photo = document.getElementById('image').value;
+
+  //Creo un oggetto con tutti i valori necessari
+  const newTeamMember = {
+    name: name,
+    role: role,
+    photo: photo
+  }
+
+   //Invio il nuovo oggetto a drawTeamMember
+   drawTeamMember(newTeamMember);
+
+   //Aggiungo l'elemento alla base dati
+   team.push(newTeamMember);
+
+  //  Resetto il form
+  resetForm()
+
+  function resetForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('role').value = '';
+    document.getElementById('image').value = '';
+  }
+ }
